bf0be480bf67ff2d67580250aa4f01d83c652fcb
Andrii Zhabynets <a.zhabynets03@gmail.com>
2022-12-14 05:43:21 +0200

    ToDoApp

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -5,18 +5,4 @@ export const getTodos = (userId: number) => {
   return client.get<Todo[]>(`/todos?userId=${userId}`);
 };
 
-export const createTodo = (userId: number, title: string) => {
-  return client.post<Todo>('/todos', {
-    userId,
-    completed: false,
-    title,
-  });
-};
-
-export const deleteTodos = (todoId: number) => {
-  return client.delete(`/todos/${todoId}`);
-};
-
-export const updateTodo = (todoID: number, data: Partial<Todo>) => {
-  return client.patch<Todo>(`/todos/${todoID}`, data);
-};
+// Add more methods here
